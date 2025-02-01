97569e4e0778e46276f8388db347b80a0ff54a7d
suryakarun <suryakarnakaran@gmail.com>
2025-02-01 23:27:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function moveSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    const slides = document.getElementsByClassName("carousel-slide");
+    const dots = document.getElementsByClassName("dot");
+    
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+        dots[i].classList.remove("active");
+    }
+    
+    slides[slideIndex-1].classList.add("active");
+    dots[slideIndex-1].classList.add("active");
+}
